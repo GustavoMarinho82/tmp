6c8812df32487891da2603c2ae1e54ebab486797
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2025-07-11 21:47:14 -0300

    commit

---
+++ P4n/P4nX.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+import ui.*;
+
+public class P4nX {
+	public static void main(String[] args) {
+/*		JFrame janelaPrincipal = new JFrame("Janela principal");
+		janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		janelaPrincipal.setSize(400, 400);
+		janelaPrincipal.setVisible(true);		
+*/	
+	TelaInicial telaInicial = new TelaInicial();
+	telaInicial.setVisible(true);
+
+	}
+}
